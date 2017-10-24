aa07a6c565c2004af55dfb2859a56a106433f755
GGG KILLER <gggkiller2@gmail.com>
2017-10-24 15:29:14 -0200

    Initial commit

---
+++ MarkovChains/MarkovChain.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MarkovChains
+{
+	public struct WordNode
+	{
+		public String Word;
+		public IList<WordNode> NextWords;
+	}
+
+	public class MarkovChain
+	{
+		private readonly Dictionary<String, WordNode> WordList = new Dictionary<String, WordNode> ( );
+
+		public WordNode GetWord ( String word )
+		{
+			return this.WordList.ContainsKey ( word ) ?
+				this.WordList[word] :
+				this.WordList[word] = new WordNode
+				{
+					Word = word,
+					NextWords = new List<WordNode> ( )
+				};
+		}
+
+		public void Learn ( String sentence )
+		{
+			String[] words = sentence.Split ( new[] { ' ' } );
+			for ( var i = 0 ; i < words.Length - 1 ; i++ )
+			{
+				WordNode word = GetWord ( words[i] );
+				WordNode next = GetWord ( words[i + 1] );
+
+				if ( !word.NextWords.Contains ( next ) )
+					word.NextWords.Add ( next );
+			}
+		}
+
+		public void DebugPrint ( )
+		{
+			Console.WriteLine ( '{' );
+			foreach ( KeyValuePair<String, WordNode> wordPair in this.WordList )
+			{
+				Console.WriteLine ( "\t" + wordPair.Key );
+				Console.WriteLine ( "\t{" );
+				foreach ( WordNode next in wordPair.Value.NextWords )
+				{
+					Console.WriteLine ( "\t\t" + next.Word );
+				}
+				Console.WriteLine ( "\t}" );
+			}
+			Console.WriteLine ( '}' );
+		}
+
+		public void Forget ( String word )
+		{
+			this.WordList[word].NextWords.Clear ( );
+			this.WordList.Remove ( word );
+		}
+
+		#region Generating
+
+		public IEnumerable<String> Generate ( )
+		{
+			var word = new List<String> ( this.WordList.Keys )[new Random ( ).Next ( 0, this.WordList.Count )];
+			return Generate ( word, Int32.MaxValue );
+		}
+
+		public IEnumerable<String> Generate ( String firstWord )
+		{
+			return Generate ( firstWord, Int32.MaxValue );
+		}
+
+		public IEnumerable<String> Generate ( String firstWord, Int32 wordCount )
+		{
+			var count = 0;
+			var rand = new Random ( );
+			WordNode current = GetWord ( firstWord );
+
+			yield return current.Word;
+			while ( ++count < wordCount )
+			{
+				if ( this.WordList.ContainsKey ( current.Word ) && current.NextWords.Count > 0 )
+				{
+					var nextIdx = rand.Next ( 0, current.NextWords.Count );
+					current = current.NextWords[nextIdx];
+					yield return current.Word;
+				}
+				else
+					break;
+			}
+		}
+
+		#endregion Generating
+
+		#region Save/Loading
+
+		public void Save ( Stream stream )
+		{
+			using ( var writer = new StreamWriter ( stream ) )
+			{
+				foreach ( KeyValuePair<String, WordNode> node in this.WordList )
+				{
+					writer.WriteLine ( node.Key );
+					foreach ( WordNode next in node.Value.NextWords )
+					{
+						writer.Write ( '\t' );
+						writer.WriteLine ( next.Word );
+					}
+				}
+			}
+		}
+
+		public void Load ( Stream stream )
+		{
+			this.WordList.Clear ( );
+			using ( var reader = new StreamReader ( stream ) )
+			{
+				String line, currentWord = null;
+				while ( ( line = reader.ReadLine ( ) ) != null )
+				{
+					var issub = line[0] == '\t';
+					line = line.Trim ( );
+					if ( issub )
+					{
+						if ( currentWord == null )
+							throw new Exception ( "Invalid save file. (no word before next word candidates)" );
+
+						WordNode word = this.GetWord ( line );
+						this.WordList[currentWord].NextWords.Add ( word );
+					}
+					else
+					{
+						currentWord = line;
+						this.WordList[currentWord] = this.GetWord ( currentWord );
+					}
+				}
+			}
+		}
+
+		#endregion Save/Loading
+	}
+}
---
+++ MarkovConsole/Program.cs
@@ -0,0 +1,90 @@
+﻿using MarkovChains;
+using System;
+using System.IO;
+
+namespace MarkovConsole
+{
+	class Program
+	{
+		static void Main()
+		{
+			var chain = new MarkovChain ( );
+			while (true)
+			{
+				Console.Write ( "markov> " );
+				var line = Console.ReadLine ( );
+				var firstSpace = line.IndexOf ( ' ' );
+				var cmd = line.Substring ( 0, firstSpace != -1 ? firstSpace : line.Length );
+				var data = firstSpace != -1 ? line.Substring ( firstSpace + 1 ) : "";
+
+				switch ( cmd )
+				{
+					case "learn":
+						chain.Learn ( data );
+						break;
+
+					case "learnfile":
+						foreach ( String fline in File.ReadAllLines ( data.Trim ( ) ) )
+							chain.Learn ( fline );
+						break;
+
+					case "generate":
+						try
+						{
+							if ( data == "" )
+							{
+								foreach ( var word in chain.Generate ( ) )
+									Console.Write ( word + " " );
+								Console.WriteLine ( );
+							}
+							else
+							{
+								String[] parts = data.Split(new[]{' '});
+								var firstWord = parts[0];
+								var length = Int32.Parse ( parts[1] );
+								foreach ( var word in chain.Generate ( firstWord, length ) )
+									Console.Write ( word + " " );
+								Console.WriteLine ( );
+							}
+						}
+						catch ( Exception e )
+						{
+							Console.WriteLine ( e );
+						}
+						break;
+
+					case "save":
+						try
+						{
+							using ( FileStream file = File.OpenWrite ( data ) )
+								chain.Save ( file );
+						}
+						catch ( Exception e )
+						{
+							Console.WriteLine ( e );
+						}
+						break;
+
+					case "load":
+						try
+						{
+							using ( FileStream file = File.OpenRead ( data ) )
+								chain.Load ( file );
+						}
+						catch ( Exception e )
+						{
+							Console.WriteLine ( e );
+						}
+						break;
+
+					case "print":
+						chain.DebugPrint ( );
+						break;
+
+					case "exit":
+						return;
+				}
+			}
+		}
+	}
+}
